4ebe76d61e28609f63bff49c538dd2d42332ef23
Matt Corbett <mcorbett@projectronin.com>
2022-02-03 07:35:35 -0500

    Add test tasks to base.gradle

--- src/main/kotlin/com/projectronin/services/gradle/base.gradle.kts
+++ src/main/kotlin/com/projectronin/services/gradle/base.gradle.kts
@@ -5,14 +5,38 @@ plugins {
     id("com.projectronin.interop.gradle.publish")
 }
 
+val kotlinVersion = "1.6.10"
+
 dependencyManagement {
     imports {
-        mavenBom("org.jetbrains.kotlin:kotlin-bom:1.5.31")
+        mavenBom("org.jetbrains.kotlin:kotlin-bom:$kotlinVersion")
         mavenBom("org.springframework.boot:spring-boot-dependencies:2.6.3")
         mavenBom("io.ktor:ktor-bom:1.6.7")
     }
 
     dependencies {
         dependency("io.github.microutils:kotlin-logging-jvm:2.1.21")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.register<Test>("integrationTest") {
+    group = "Verification"
+    description = "Run integration tests matching format *IntegrationTest"
+    filter {
+        isFailOnNoMatchingTests = false
+        includeTestsMatching("*IntegrationTest")
+    }
+}
+
+tasks.register<Test>("unitTest") {
+    group = "Verification"
+    description = "Run unit tests matching format *UnitTest"
+    filter {
+        isFailOnNoMatchingTests = false
+        includeTestsMatching("*UnitTest")
     }
 }
