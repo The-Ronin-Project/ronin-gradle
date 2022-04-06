94ed60fbfd9424ad67a33b815b8dd1cfeb16da12
Corbin Hughes <corbin@projectronin.com>
2022-04-06 18:36:09 -0500

    Move to new repo and remove Jib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,10 @@
 plugins {
     `kotlin-dsl`
 
-    id("com.projectronin.interop.gradle.base") version "1.0.0-SNAPSHOT"
-    id("com.projectronin.interop.gradle.junit") version "1.0.0-SNAPSHOT"
-    id("com.projectronin.interop.gradle.jacoco") version "1.0.0-SNAPSHOT"
-    id("com.projectronin.interop.gradle.publish") version "1.0.0-SNAPSHOT"
-}
-
-repositories {
-    maven {
-        name = "ronin"
-        url = uri("https://maven.pkg.github.com/projectronin/package-repo")
-        credentials {
-            username = System.getenv("PACKAGE_USER")
-            password = System.getenv("PACKAGE_TOKEN")
-        }
-    }
-    mavenCentral()
-    maven(url = "https://plugins.gradle.org/m2/")
+    id("com.projectronin.interop.gradle.base")
+    id("com.projectronin.interop.gradle.junit")
+    id("com.projectronin.interop.gradle.jacoco")
+    id("com.projectronin.interop.gradle.publish")
 }
 
 dependencies {
@@ -29,16 +16,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.1")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
 
-    implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.1.4")
     implementation("de.undercouch:gradle-download-task:4.1.2")
-}
-
-// ktlint includes the generated-sources, which includes the classes created by Gradle for these plugins
-ktlint {
-    enableExperimentalRules.set(true)
-    filter {
-        // We should be able to just do a wildcard exclude, but it's not working.
-        // This solution comes from https://github.com/JLLeitschuh/ktlint-gradle/issues/222#issuecomment-480758375
-        exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated-sources/") }
-    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,34 @@
 rootProject.name = "ronin-gradle"
 
 pluginManagement {
+    val interopGradleVersion: String by settings
+    plugins {
+        id("com.projectronin.interop.gradle.base") version interopGradleVersion
+        id("com.projectronin.interop.gradle.junit") version interopGradleVersion
+        id("com.projectronin.interop.gradle.jacoco") version interopGradleVersion
+        id("com.projectronin.interop.gradle.publish") version interopGradleVersion
+    }
+
     repositories {
         maven {
-            url = uri("https://maven.pkg.github.com/projectronin/package-repo")
-            credentials {
-                username = System.getenv("PACKAGE_USER")
-                password = System.getenv("PACKAGE_TOKEN")
+            url = uri("https://repo.devops.projectronin.io/repository/maven-snapshots/")
+            mavenContent {
+                snapshotsOnly()
             }
         }
-        mavenCentral()
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-releases/")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        maven {
+            url = uri("https://repo.devops.projectronin.io/repository/maven-public/")
+            mavenContent {
+                releasesOnly()
+            }
+        }
+        mavenLocal()
         gradlePluginPortal()
     }
 }
