adc1d4b5faa4dcdd68013dfde252d4391bd7bc67
Ross M. Lodge <ross@projectronin.com>
2023-07-08 15:26:42 -0700

    Really only remove them from coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -135,7 +135,7 @@ sonar {
     properties {
         property("sonar.projectKey", project.name)
         property("sonar.projectName", project.name)
-        property("sonar.exclusions", "**/test/**")
+        property("sonar.coverage.exclusions", "**/test/**")
         property("sonar.coverage.jacoco.xmlReportPaths", layout.buildDirectory.file("reports/jacoco/testCodeCoverageReport/testCodeCoverageReport.xml").get())
     }
 }
